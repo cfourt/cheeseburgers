5d1f5ada20a1be4af4f4769d72d802b17fff3740
Connor <connorfourt@gmail.com>
2016-07-14 19:32:41 -0700

    Initial Commit

---
+++ Cheeseburgers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cheeseburgers
+//
+//  Created by Connor Fourt on 7/14/16.
+//  Copyright © 2016 Connor Fourt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cheeseburgers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cheeseburgers
+//
+//  Created by Connor Fourt on 7/14/16.
+//  Copyright © 2016 Connor Fourt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
